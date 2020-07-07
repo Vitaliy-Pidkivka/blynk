2114533e0879b4b8ea485609167b02f340b89ce2
vpidkivka <vpidkivka@eliteit.org>
2020-07-07 12:50:51 +0300

    Updates

---
+++ precache-manifest.a0c040f2844a55c1c2f9df76f79a6fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442134484615af35325896ca10da8256",
+    "url": "/blynk/index.html"
+  },
+  {
+    "revision": "74f235cec0d91ff52a4b",
+    "url": "/blynk/static/css/main.055880af.chunk.css"
+  },
+  {
+    "revision": "56d5e81e4fe83c92776a",
+    "url": "/blynk/static/js/2.57d8ce17.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/blynk/static/js/2.57d8ce17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f235cec0d91ff52a4b",
+    "url": "/blynk/static/js/main.7a958ecb.chunk.js"
+  },
+  {
+    "revision": "5d354db9a8805d2e238f",
+    "url": "/blynk/static/js/runtime-main.05020fc3.js"
+  }
+]);
